4f90249c4dcdf9632419ac2c9e4a1d25d96c1ba3
DT <52263564+delsonting@users.noreply.github.com>
2025-10-17 17:11:03 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // If deploying to USERNAME/waypoint-site on GitHub Pages:
-  base: '/waypoint-site/',
-  // If using your custom domain (waypointoperating.com), change to:
-  // base: '/'
+  base: '/waypoint-site/', // repo name
 })
