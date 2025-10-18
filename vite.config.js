c3e7879d442d7bd4a3303f5b9b9022b76e496757
DT <52263564+delsonting@users.noreply.github.com>
2025-10-17 17:23:02 -0700

    setup pages for hosting

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/waypoint-site/', // repo name
+  //base: '/waypoint-site/',   // repo name
+  base: '/',   // custom domain
+  build: { outDir: 'docs' }  // GitHub Pages can serve /docs
 })
